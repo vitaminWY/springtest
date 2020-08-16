47a20bafd0a597cc4033b5a68a16f6b9aeb5bf4a
wangyu <super0506wy@163.com>
2020-08-16 17:13:07 +0800

    new feature01 branch commit

--- src/main/java/com/wy/app/Feature01.java
+++ src/main/java/com/wy/app/Feature01.java
@@ -3,5 +3,8 @@ package com.wy.app;
 public class Feature01 {
     public static void main(String[] args) {
         System.out.println("this is new feature01 branch");
+
+        System.out.println("this is merging to  master");
+        ss
     }
 }
